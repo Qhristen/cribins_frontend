2694f35871f72624f56d40b8ccbb4dd6b331a3d9
Qhristen <odeyxtian@gmail.com>
2024-08-13 12:36:40 +0100

    update logo

---
+++ assets/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as LogoLight } from "./logo_light.svg";
+export { default as LogoDark } from "./logo.svg";
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,10 @@
 
 'use client';
 
+import { LogoDark, LogoLight } from '@/assets/icons';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import React from 'react';
-import Logo_light from '@/assets/images/logo_light.png'
-import Logo_Dark from '@/assets/images/logo.png'
 
 const Hero = () => {
     const { theme } = useTheme();
@@ -21,16 +20,17 @@ const Hero = () => {
         />
         <div>
           <div className='flex items-center gap-2 pb-4'>
-            <Image
-              src={theme === "light" ? Logo_Dark : Logo_light}
+            {/* <Image
+              src={theme === "light" ? <Logo_Dark /> : <Logo_light />}
               width={30}
               height={30}
               alt="cribins_logo"
-            />
-            <span className="font-bold text-2xl">Cribins</span>
+            /> */}
+            {theme === "light" ? <LogoDark /> : <LogoLight  />}
+            {/* <span className="font-bold text-2xl">Cribins</span> */}
           </div>
           <h1 className="text-4xl font-black">
-            Buy, rent, or sell <br /> your property easily
+            Buy, rent, or sell <br /> your property <br /> easily.
           </h1>
 
           <p className='pt-4 text-sm'>A great platform to buy, sell, or even <br /> rent your properties without any commisions.</p>
